88a540f7738011fab92765b64924f12ec64e8ec2
Masamine <Masamine@MacBook-Air.local>
2015-06-14 07:21:32 +0900

    失敗したAM630の代わり

--- batoran/platforms/ios/www/js/script.js
+++ batoran/platforms/ios/www/js/script.js
@@ -1,11 +1,56 @@
 (function($){
-    $(document).on({
-        "ready" : setMenu
+    $(window).on({
+        "load" : setMenu
     });
 
     function setMenu() {
+        var target = $(".j-listview-item-content");
+
+        target.on("click", showMenu);
+        target.find(".heart").on("click", setFavorite);
+
+        function showMenu() {
+
+          var $this      = $(this);
+          var name       = $this.find(".name").text();
+          var price      = $this.find(".price").text();
+          var orderPrice = Number(price.split("￥")[1].split(',').join('').trim());
+          var orderTotal = (loadData("order")) ? loadData("order") : [];
+          var order      = {
+            "name"  : name,
+            "price" : orderPrice
+          }
+
+          orderTotal.push(order);
+          
+          if(confirm(name + '(' +price + ')を注文しますか？')) {
+            alert("注文が完了しました。");
+            localStorage.setItem("order", JSON.stringify(orderTotal));
+          }
+          
+          return false;
+        }
         
         return false;
+    }
+
+    function setFavorite() {
+      var $this = $(this);
+
+      if($this.hasClass("favorite")) {
+        $this.removeClass("favorite");
+      } else {
+        $this.addClass("favorite");
+      }
+
+      return false;
+    }
+
+    /* -------------------------
+    Load Data
+    ------------------------- */
+    function loadData(id) {
+      return JSON.parse(localStorage.getItem(id));
     }
 
 })(jQuery)
